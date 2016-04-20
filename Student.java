2a470dcd0b81ba6fa118730615e68493edc098b6
Ivanna <i.gulyma@gmail.com>
2016-04-20 11:59:15 +0300

    created Student Class

---
+++ Student.java
@@ -0,0 +1,26 @@
+
+public class Student {
+	public String sName = "";
+    public int iGrade = 0;
+    public int iAge = 0;
+	
+    public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public int getiGrade() {
+		return iGrade;
+	}
+	public void setiGrade(int iGrade) {
+		this.iGrade = iGrade;
+	}
+	public int getiAge() {
+		return iAge;
+	}
+	public void setiAge(int iAge) {
+		this.iAge = iAge;
+	}
+    
+}
